bac5f7d2f268fffbf17dc464ad3520bb0dc6b43b
Hubert Andrzejewski <antrof@gmail.com>
2024-05-09 11:24:45 +0200

    Initial Commit

---
+++ DeDuP/DeDuPApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeDuPApp.swift
+//  DeDuP
+//
+//  Created by Hubert Andrzejewski on 09/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct DeDuPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
